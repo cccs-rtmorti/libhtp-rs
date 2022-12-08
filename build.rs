1c39f70eb29e681be7ea4e7b48f8412c11139a7e
Todd Mortimer <richard.mortimer@cyber.gc.ca>
2022-12-08 19:31:01 +0000

    cbindgen: Add option for headers output directory.
    When this library is built with cbindgen support as a dependency,
    the output location for the generated headers is hard for the parent
    program to predict and fetch. So provide an environment variable
    the caller can set beforehand to just specify the header output
    directory.
    
    eg: CBINDGEN_HEADERS_DIR=/path/to/headers cargo build ...

--- build.rs
+++ build.rs
@@ -1,12 +1,3 @@
-#[cfg(feature = "cbindgen")]
-extern crate cbindgen;
-
-#[cfg(feature = "cbindgen")]
-use std::env;
-
-#[cfg(feature = "cbindgen")]
-use std::path::PathBuf;
-
 fn common_actions() {
     println!("cargo:rustc-flags=-l z");
     cdylib_link_lines::metabuild();
@@ -19,22 +10,31 @@ fn main() {
 
 #[cfg(feature = "cbindgen")]
 fn main() {
+    extern crate cbindgen;
+    use std::env;
+    use std::path::PathBuf;
+
     common_actions();
 
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let hdr_path = if let Ok(path) = env::var("CBINDGEN_HEADERS_DIR") {
+        PathBuf::from(path)
+    } else {
+        PathBuf::from(crate_dir.clone())
+    };
+    let htp_h_path = hdr_path.join("htp/htp.h");
+    let version_h_path = hdr_path.join("htp/version.h");
 
     cbindgen::generate_with_config(
         crate_dir.clone(),
-        cbindgen::Config::from_root_or_default(crate_dir.clone()),
+        cbindgen::Config::from_root_or_default(crate_dir),
     )
     .expect("Unable to generate bindings")
-    .write_to_file("htp/htp.h");
+    .write_to_file(htp_h_path);
 
     // Write a version.h to include with the c_api
-    let mut hdr_path = PathBuf::from(crate_dir);
-    hdr_path.push("htp/version.h");
     std::fs::write(
-        hdr_path,
+        version_h_path,
         format!(
             "#define HTP_VERSION_STRING_FULL \"LibHTP v{}\"\n",
             env!("CARGO_PKG_VERSION")
